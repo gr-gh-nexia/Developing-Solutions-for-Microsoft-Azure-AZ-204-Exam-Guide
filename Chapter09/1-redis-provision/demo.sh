dac5b38cf2543938903db32599865edd6b66884c
G. Robert <gr_gh@grnexiacomp.net>
2024-01-05 22:56:04 +0400

    fixed line endings and syntax

--- Chapter09/1-redis-provision/demo.sh
+++ Chapter09/1-redis-provision/demo.sh
@@ -1,18 +1,15 @@
 ################################################################
 # These commands should be executed from Azure Cloud Shell bash 
 ################################################################
-
 #create the resource group
-az group create --location eastus2 --name rg-az204-ch09-AzureCache
-
+rgname=rg-az204-ch09-AzureCache
+myloc=southafricanorth
+az group create --location $myloc --name $rgname
 # to avoid name collisions generate a unique name for your account
 account=azurecache$RANDOM
-
 #create Basic SKU instance
-az redis create --location eastus2 --name $account --resource-group AzureCache-RG --sku Basic --vm-size C0
-
+az redis create --location $myloc --name $account --resource-group $rgname --sku Basic --vm-size C0
 #retrieve key and address 
-$key=$(az redis list-keys --name $account --resource-group AzureCache-RG --query primaryKey -o tsv)
+$key=$(az redis list-keys --name $account --resource-group $rgname --query primaryKey -o tsv)
 echo $key
 echo $account.redis.cache.windows.net
-
